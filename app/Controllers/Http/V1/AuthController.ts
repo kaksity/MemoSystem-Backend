0fc2097078f698175487fba75964272f2056e451
Dauda Pona <daudapona2@gmail.com>
2023-01-28 16:25:10 +0100

    chore: clean up the authentication controller

--- app/Controllers/Http/V1/AuthController.ts
+++ app/Controllers/Http/V1/AuthController.ts
@@ -1,12 +1,11 @@
 import { inject } from '@adonisjs/core/build/standalone'
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import LoginValidator from 'App/Validators/Auth/LoginValidator'
 import RegistrationValidator from 'App/Validators/Auth/RegistrationValidator'
-import  AuthService from 'App/Services/AuthService'
+import AuthService from 'App/Services/AuthService'
 import UserService from 'App/Services/UserService'
 import NotFoundException from 'App/Exceptions/NotFoundException'
 import AlreadyExistException from 'App/Exceptions/AlreadyExistException'
-import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import BadRequestException from 'App/Exceptions/BadRequestException'
 import ChangePasswordValidator from 'App/Validators/Auth/ChangePasswordValidator'
 import UserResource from 'App/Resources/User/UserResource'
@@ -69,18 +68,23 @@ export default class AuthController {
   public async changePassword({ request, response, auth }: HttpContextContract) {
     await request.validate(ChangePasswordValidator)
     const { oldPassword, newPassword } = request.body()
-    
+
     const user = auth.user!
 
-    if(await this.authService.verifyPassword({ password: oldPassword, passwordHash: user.password}) === false) {
+    if (
+      (await this.authService.verifyPassword({
+        password: oldPassword,
+        passwordHash: user.password,
+      })) === false
+    ) {
       throw new BadRequestException('Old Password is not correct')
     }
     user.password = await this.authService.hashPassword(newPassword)
-    
+
     await this.userService.updateUser(user)
     return response.json({
       success: true,
-      message: 'Password was changed successfully'
+      message: 'Password was changed successfully',
     })
   }
 }
